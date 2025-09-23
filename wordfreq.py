2ace156532eaa8fe4bf65d3b30a4380d6c2f2090
NilsFriman <74624397+NilsFriman@users.noreply.github.com>
2025-09-23 13:40:49 +0200

    Nils arbete under SLU
    Det jag gjorde under SLU.
    Funktionen tokenization() är nästan klar, men har fortfarande en bugg. Efter specialtecken läggs en tom sträng till i listan med tokens.

---
+++ wordfreq.py
@@ -0,0 +1,54 @@
+def tokenize(document):
+
+    words = []  # lowercase, separated using only spaces.
+
+    for line in document:
+        for word in line.lower().split():
+            words.append(word)
+
+    # We still need to handle words with letters AND numbers, as well as special characters.
+
+    # A help function to find the type of character
+    def char_type(checking_character):
+
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        numbers = "0123456789"
+
+        if checking_character in letters:
+            return "letter"
+        if checking_character in numbers:
+            return "number"
+        return "special"
+
+    tokens = []
+
+    for word in words:
+
+        token = ""
+        token_type = None
+
+        for character in word:
+
+            character_type = char_type(character)
+            
+            if character_type == token_type:
+                token += character
+                continue
+            
+            if token:
+                tokens.append(token)
+                token = character
+                token_type = character_type
+
+                if token_type == "special":
+                    tokens.append(token)
+                    token = ""
+                    token_type = None
+            
+            else:  # New token
+                token = character
+                token_type = character_type
+        
+        tokens.append(token)
+    
+    return tokens
