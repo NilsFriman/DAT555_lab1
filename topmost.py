690671cb51274737ed94306554788cdaeb15e2f6
NilsFriman <74624397+NilsFriman@users.noreply.github.com>
2025-09-30 15:11:43 +0200

    La till URL-funktionalitet och kommentarer

--- topmost.py
+++ topmost.py
@@ -1,20 +1,28 @@
 from wordfreq import tokenize, countWords, printTopMost
 from sys import argv
+from urllib import request
 
 def main(arguments):
-
+    
+    # with open() does the same as opening and then using close()
     with open(arguments[1], encoding="UTF-8") as file:
         stopwords = []
         for word in file.readlines():
             stopwords.append(word.strip())
     
-    with open(arguments[2], encoding="UTF-8") as file:
-        document = []
-        for line in file.readlines():
-            document.append(line.strip())
+    if arguments[2].find("http://") == 0 or arguments[2].find("https://") == 0:  # URL link supplied
+        response = request.urlopen(arguments[2])
+        document = response.read().decode("UTF-8").splitlines()
+    else:   
+        # with open() does the same as opening and then using close()
+        with open(arguments[2], encoding="UTF-8") as file:
+            document = file.readlines()
+
+    # We will print the top n most frequent tokens
+    n = int(arguments[3])
 
     tokens = tokenize(document)
     frequencies = countWords(tokens, stopwords)
-    printTopMost(frequencies, int(arguments[3]))
+    printTopMost(frequencies, n)
 
 main(argv)
